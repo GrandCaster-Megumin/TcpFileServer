c1aeb5647f8de90ad8f2648fb85fa1ef8d5bb74a
miku <40943258@gm.nfu.edu.tw>
2024-11-25 14:08:46 +0800

    combine with tcpfilesender in class

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-#include "tcpfileserver.h"
+﻿#include "tcpfileserver.h"
 #include <QApplication>
-
+#include<tcpfilesender.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     TcpFileServer w;
+    TcpFileSender x;
+    x.show();
     w.show();
 
     return a.exec();
---
+++ tcpfilesender.h
@@ -0,0 +1,43 @@
+﻿#ifndef TCPFILESENDER_H
+#define TCPFILESENDER_H
+
+#include <QDialog>
+#include <QtNetwork>
+#include <QtWidgets>
+#include <QLineEdit>
+
+class TcpFileSender : public QDialog
+{
+    Q_OBJECT
+
+public:
+    TcpFileSender(QWidget *parent = 0);
+    ~TcpFileSender();
+public slots:
+    void start();
+    void startTransfer();
+    void updateClientProgress(qint64 numBytes);
+    void openFile();
+private:
+    QProgressBar     *clientProgressBar;
+    QLabel           *clientStatusLabel;
+    QPushButton      *startButton;
+    QPushButton      *quitButton;
+    QPushButton      *openButton;
+    QDialogButtonBox *buttonBox;
+    QTcpSocket       tcpClient;
+    QLabel           *hostLabel;
+    QLineEdit        *lineEdit;
+    QLabel           *portLabel;
+    QLineEdit        *lineEdit2;
+
+    qint64           totalBytes;
+    qint64           bytesWritten;
+    qint64           bytesToWrite;
+    qint64           loadSize;
+    QString          fileName;
+    QFile            *localFile;
+    QByteArray       outBlock;
+};
+
+#endif // TCPFILESENDER_H
